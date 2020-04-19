230e3320cf08c48b9085745282092d7b7b9b0e0f
Areen-alshwayat <61476000+Areen-alshwayat@users.noreply.github.com>
2020-04-19 18:47:45 +0300

    Update setup-environment.sh

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,4 +1,5 @@
 sudo apt-get install python3-venv
 sudo apt-get install python3-pip
-python3 -m venv test_inv && source test_inv/bin/activate && pip3 install -r requirements.txt && pip3 install gunicorn
 gunicorn --bind=0.0.0.0:1742 --workers=4 hello:app
+
+
