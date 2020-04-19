737030fd60c31fdfb0db3a22c1dfaee3a75a100d
Areen-alshwayat <61476000+Areen-alshwayat@users.noreply.github.com>
2020-04-19 19:50:46 +0300

    Update and rename setup-environment.sh to Install Python.sh

--- setup-environment.sh
+++
@@ -1,9 +0,0 @@
-#
-sudo apt-get install python3-venv 
-python3 -m venv .env
-source .env/bin/activate
-pip install -r requirements.txt
-export set FLASK_APP=hello_app.webapp
-python3 -m flask run
-
-
