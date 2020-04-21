c795cc4ec03d20b308854579683e830a88d00a4a
Areen-alshwayat <61476000+Areen-alshwayat@users.noreply.github.com>
2020-04-21 13:54:05 +0300

    Update and rename Install Python.sh to run_sever.sh

--- Install Python.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get -y install python3 python3-venv python3-dev
-python -version
-sudo apt-get -y install nginx git
-sudo apt-get -y install git
-
-echo"areen"
-
-
-
---
+++ run_sever.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+export FLASK_APP=hello.py
+# export FLASK_DEBUG=1 
+flask run -h 0.0.0.0
+
+source venv/bin/activate
+export FLASK_APP=hello.py
+flask run -h 0.0.0.0
+
+
+
