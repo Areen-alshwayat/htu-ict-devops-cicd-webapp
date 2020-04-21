285fb01297ac7d15553cd31607339084ca6de883
Areen-alshwayat <61476000+Areen-alshwayat@users.noreply.github.com>
2020-04-21 12:44:41 +0300

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -3,7 +3,7 @@ open the git repo
 git pull
 
 source venv/bin/activate
-export FLASK_APP=application.py
+export FLASK_APP=hello.py
 flask run -h 0.0.0.0
 
 gunicorn --bind=0.0.0.0:1742 --workers=4 hello:app
